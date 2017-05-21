ba4b889dc0d472ad2a22ad95edf7f52e4514da6f
Ivan Vegner <ivanvegner@gmail.com>
2017-05-20 23:17:58 -0400

    Added game controller boilerplate

---
+++ controllers/games.js
@@ -0,0 +1,19 @@
+const Game = require('../models/schemas/game');
+const mongoose = require('mongoose');
+
+// POST /game {name, users:[user1, user2, user3]}
+exports.makeGame = (req, res, next) => {
+    return res.send(200);
+};
+
+// PUT /game/<game_id> {name: new_name}. Not crucial
+exports.updateGame = (req, res, next) => {
+    return res.send(200);
+};
+
+//exports.deleteGame
+
+// GET /game/<game_id> 
+exports.getGame = (req, res, next) => {
+    return res.send(200);
+};
