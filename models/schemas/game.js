16bb1dc390230b756dce946abc283b245634921b
eagraf <graf.ethan@gmail.com>
2017-05-21 01:59:17 -0400

    Made sure join codes are not replicated

--- models/schemas/game.js
+++ models/schemas/game.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 var gameSchema = new Schema({
         name: {type: String, required: true, trim: true},
-        joinCode: {type: Number},
+        joinCode: {type: Number, unique: true},
         users: {type: [Schema.objectId], ref: User},
         beacons: [{
             lat: Number,
